2390d0d547ed9eb795c8a6cb144b5a49f55b5241
ruanfangqi <2468opq@sina.com>
2019-11-20 12:30:00 +0800

    ip

--- src/main/java/com/orange/voice/controller/HelloWorldController.java
+++ src/main/java/com/orange/voice/controller/HelloWorldController.java
@@ -1,9 +1,12 @@
 package com.orange.voice.controller;
 
+import com.orange.voice.util.IpUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/hello")
@@ -18,5 +21,12 @@ public class HelloWorldController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
         return "/hello";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(HttpServletRequest request) {
+        String ip = IpUtil.getIpAddr(request);
+        return ip;
     }
 }
