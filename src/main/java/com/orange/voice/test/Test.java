f2e295fd0d008939cdb6c94f4277ff7e4099dc26
ruanfangqi <2468opq@sina.com>
2020-01-08 15:07:39 +0800

    voice

--- src/main/java/com/orange/voice/test/Test.java
+++ src/main/java/com/orange/voice/test/Test.java
@@ -1,15 +1,56 @@
 package com.orange.voice.test;
 
 import com.orange.voice.util.SecurityUtil;
+import javafx.scene.media.AudioTrack;
+import org.springframework.util.ResourceUtils;
 
+import javax.sound.sampled.AudioFormat;
+import java.io.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
 
+    private File tempFile = null;
+
     public static void main(String[] args) {
         String s = "123A";
         System.out.println(SecurityUtil.getMD5(s));
+        //AudioTrack audioTrack = new AudioTrack()
+        try {
+            //String outUrl = ResourceUtils.getURL("classpath:static").getPath().replace("%20", " ").substring(1);
+
+            String outUrl = "./src/main/resources/static";
+            System.out.println(outUrl);
+            //md5 -> base64 -> .mp3
+            SecurityUtil.decoderBase64File(SecurityUtil.getMD5(s), outUrl + "/" + "1.mp3");
+
+            //.mp3 -> base64
+            String out = SecurityUtil.encodeBase64File(outUrl + "/1.mp3");
+            System.out.println(out);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+//    public void start(int rate){
+//        if(rate > 0){
+//            int Hz = rate;
+//            int waveLen = 44100/ Hz;
+//            int length = waveLen * Hz;
+//            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+//                    AudioFormat.CHANNEL_CONFIGURATION_STEREO, // CHANNEL_CONFIGURATION_MONO,
+//                    AudioFormat.ENCODING_PCM_8BIT,length,AudioTrack.MODE_STREAM);
+//            //生成正弦波
+//            wave = SinWave.sin(wave, waveLen, length);
+//            if(audioTrack!=null){
+//                audioTrack.play();
+//            }
+//        }else{
+//            return;
+//        }
+//    }
 
     public static void func() {
         AtomicInteger i = new AtomicInteger(2000000);
@@ -22,4 +63,53 @@ public class Test {
         a.getAndIncrement();
     }
 
+    /**
+     * 将base64字符转换成临时音乐文件
+     */
+//    private File base64ToFile(String base64Str) {
+//        FileOutputStream outputStream = null;
+//        if (tempFile == null || !tempFile.exists())
+//            try {
+//                tempFile = File.createTempFile("temp", ".mp3");
+//                byte[] audioByte = Base64.decode(base64Str, Base64.DEFAULT);
+//                if (tempFile != null) {
+//                    outputStream = new FileOutputStream(tempFile);
+//                    outputStream.write(audioByte, 0, audioByte.length);
+//                    outputStream.flush();
+//                    outputStream.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    if (outputStream != null) {
+//                        outputStream.flush();
+//                        outputStream.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        return tempFile;
+//    }
+
+    /**
+     * 将本地audio文件转为base64字符串
+     * @param context
+     * @return
+     * @throws Exception
+     */
+//    public String encodeBase64File(Context context) throws Exception {
+//        //R.raw.test：在raw资源文件夹下放入测试声音文件
+//        InputStream inputStream = context.getResources().openRawResource(R.raw.test);
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        byte[] buffer = new byte[1024];
+//        int len = 0;
+//        while ((len = inputStream.read(buffer)) != -1) {
+//            outputStream.write(buffer, 0, len);
+//        }
+//        outputStream.close();
+//        inputStream.close();
+//        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+//    }
 }
--- src/main/java/com/orange/voice/util/SecurityUtil.java
+++ src/main/java/com/orange/voice/util/SecurityUtil.java
@@ -1,22 +1,53 @@
 package com.orange.voice.util;
 
+import com.orange.voice.controller.MobileController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.DigestUtils;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+//Encrypt and decrypt
 public class SecurityUtil {
 
-    //Encrypt and decrypt
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityUtil.class);
 
     //slat，used to mix md5
     private static final String slat = "=&%0525***&&%%$$#@--=";
 
     /**
      * md5
+     *
      * @param str
      * @return str
      */
     public static String getMD5(String str) {
-        String base = str +"/"+slat;
+        String base = str + "/" + slat;
         String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
         return md5;
+    }
+
+    public static String encodeBase64File(String path) throws Exception {
+        File file = new File(path);
+        FileInputStream inputFile = new FileInputStream(file);
+        byte[] buffer = new byte[(int) file.length()];
+        inputFile.read(buffer);
+        inputFile.close();
+        return new BASE64Encoder().encode(buffer);
+    }
+
+    public static void decoderBase64File(String base64Code, String targetName) throws Exception {
+        byte[] buffer = new BASE64Decoder().decodeBuffer(base64Code);
+        LOGGER.debug(targetName);
+        File file = new File(targetName);
+        LOGGER.debug(String.valueOf(file.exists()));
+        boolean res = file.createNewFile();
+        FileOutputStream out = new FileOutputStream(file);
+        out.write(buffer);
+        out.close();
     }
 }
