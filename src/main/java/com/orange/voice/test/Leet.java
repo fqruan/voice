bf06b2f9b596542ec1fc9e7a2f14abf7dacd326c
ruanfangqi <2468opq@sina.com>
2020-03-23 17:07:41 +0800

    leet

--- src/main/java/com/orange/voice/test/Leet.java
+++ src/main/java/com/orange/voice/test/Leet.java
@@ -3,14 +3,336 @@ package com.orange.voice.test;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Leet {
 
     public static void main(String[] args) {
         Leet l = new Leet();
-        int[] a = new int[]{1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0};
-        System.out.println(Arrays.toString(l.beautifulArray(10)));
+        int[] a = new int[]{1,2,3,4,5};
+        int[] b = new int[]{4,5,3,2,1};
+        String[] strings = new String[]{"doeeqiy","yabhbqe","twckqte"};
+        System.out.println(l.minDeletionSize(strings));
+//        l.test();
+    }
+
+    public void test() {
+        String s = "N3_WH_Central_West_NSW_RefYear2011.csv";
+        s = s.replaceAll("N.*_WH_.*\\.csv", "1");
+        System.out.println(s);
+    }
+
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        if (N == 0) return cells;
+        if (cells[0] == 1 || cells[7] == 1) {
+            cells = oneDay(cells);
+            cells[0] = 0;
+            cells[7] = 0;
+            N--;
+        }
+
+        int[] arr = cells;
+        int count = 0;
+
+        while (N-- > 0) {
+            arr = oneDay(arr);
+            count++;
+            if (Arrays.equals(arr, cells))
+                N %= count;
+        }
+        return arr;
+    }
+
+    private int[] oneDay(int[] cells) {
+        int[] r = new int[8];
+        for (int i = 1; i < 7; i++) r[i] = 1 - (cells[i-1] ^ cells[i+1]);
+        return r;
+    }
+
+    public int minDeletionSize(String[] A) {
+        if (A.length == 0 || A[0].length() == 0) return 0;
+        int ans = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i < A.length; i++) {
+            set.add(i);
+        }
+        for (int i = 0; i < A[0].length(); i++) {
+            HashSet<Integer> setCopy = new HashSet<>(set);
+            for (int j = 1; j < A.length; j++) {
+                if (A[j].charAt(i) > A[j-1].charAt(i)) {
+                    if (set.contains(j)) set.remove(j);
+                }else if (A[j].charAt(i) < A[j-1].charAt(i)) {
+                    if (set.contains(j)) {
+                        ans++;
+                        set = new HashSet<>(setCopy);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val != root2.val)  return false;
+        if (root1.left == null && root2.left == null && root1.right == null && root2.right == null) return true;
+        if (root1.left == null && root2.left != null && root1.right != null && root2.right == null) {
+            return flipEquiv(root1.right, root2.left);
+        }
+        if (root1.left != null && root2.left != null && root1.right == null && root2.right == null) {
+            return flipEquiv(root1.left, root2.left);
+        }
+        if (root1.left != null && root2.left == null && root1.right == null && root2.right != null) {
+            return flipEquiv(root1.left, root2.right);
+        }
+        if (root1.left == null && root2.left == null && root1.right != null && root2.right != null) {
+            return flipEquiv(root1.right, root2.right);
+        }
+        if (root1.left != null && root2.left != null && root1.right != null && root2.right != null) {
+            if (root1.left.val == root2.left.val && root1.right.val == root2.right.val) {
+                return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+            }
+            if (root1.left.val == root2.right.val && root1.right.val == root2.left.val) {
+                return flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+            }
+        }
+
+        return false;
+    }
+
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        List<Integer> al = Arrays.stream(deck).boxed().collect(Collectors.toList());
+        int s = al.size()-2;
+        List<Integer> ansList = new ArrayList<>();
+        ansList.add(al.get(al.size()-1));
+        while (s >= 0) {
+            int tm = ansList.get(ansList.size()-1);
+            ansList.remove(ansList.size()-1);
+            ansList.add(0, tm);
+            ansList.add(0, al.get(s--));
+        }
+
+        int[] ans = ansList.stream().mapToInt(Integer::valueOf).toArray();
+
+        return ans;
+    }
+
+    public int bagOfTokensScore(int[] tokens, int P) {
+        int power = P;
+        int point = 0;
+        int ans = 0;
+//        int sum = 0;
+//        for (int x : tokens) {
+//            sum += x;
+//        }
+        Arrays.sort(tokens);
+        if (tokens.length == 0 || P < tokens[0]) return 0;
+        int left = 0;
+        int right = tokens.length-1;
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                power -= tokens[left];
+                //sum -= tokens[left];
+                left++;
+                point++;
+                ans = Math.max(ans, point);
+            }else if (point == 0) {
+                return ans;
+            }else {
+                point--;
+                power += tokens[right];
+                //sum -= tokens[right];
+                right--;
+            }
+        }
+
+        return ans;
+    }
+
+    public int removeStones(int[][] stones) {
+        int[] fa = new int[stones.length];
+        int[] num = new int[stones.length];
+        for (int i = 0; i < stones.length; i++) {
+            fa[i] = i;
+            num[i] = 1;
+        }
+
+        for (int i = 0; i < stones.length; i++) {
+            for (int j = i + 1; j < stones.length; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    stonesUnion(i, j, fa, num);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (fa[i] == i && num[i] > 1) {
+                ans += num[i] - 1;
+            }
+        }
+
+        return ans > 0 ? ans : 0;
+    }
+
+    private int findStoneGroup(int x, int[] fa) {
+        if (x == fa[x]) return x;
+        return findStoneGroup(fa[x], fa);
+    }
+
+    private void stonesUnion(int x, int y, int[] fa, int[] num) {
+        int fx = findStoneGroup(x, fa);
+        int fy = findStoneGroup(y, fa);
+        if (fx != fy) {
+            fa[fy] = x;
+            num[fx] += num[fy];
+        }
+    }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int push = 0;
+        int pop = 0;
+        while (push < pushed.length || pop < popped.length) {
+            if (stack.empty() || stack.peek() != popped[pop]) {
+                if (push < pushed.length)
+                    stack.push(pushed[push++]);
+                else
+                    return false;
+            }else {
+                stack.pop();
+                pop++;
+            }
+        }
+
+        return true;
+    }
+
+    public int minIncrementForUnique2(int[] A) {
+        Queue<Integer> q = new LinkedList<>();
+        int[] container = new int[50000];
+        int ans = 0, maxNum = 0, i = 0;
+
+        for(int element : A){
+            container[element]++;
+            maxNum = Math.max(maxNum, element);
+        }
+
+        while(i < maxNum+1 || q.size() > 0){
+            if(container[i] > 1) q.add(i);
+            else if(container[i] == 0){
+                if (q.size() > 0) {
+                    int x = q.poll();
+                    ans += (i - x);
+                    container[x]--;
+                    if (container[x] != 1) q.add(x);
+                }
+            }
+            i++;
+        }
+        return ans;
+    }
+
+    public int minIncrementForUnique(int[] A) {
+        Arrays.sort(A);
+        HashSet<Integer> set = new HashSet<>();
+        int ans = 0;
+        for (int x : A) {
+            if (!set.contains(x)) {
+                set.add(x);
+            }else {
+                while (set.contains(x)) {
+                    x++;
+                    ans++;
+                }
+                set.add(x);
+            }
+        }
+        return ans;
+    }
+
+    public int minAreaRect2(int[][] points) {
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int w1 = points[i][0] - points[j][0];
+                int w2 = points[i][1] - points[j][1];
+
+                if (w1 == 0 || w2 == 0) continue;
+                int area = Math.abs(w1) * Math.abs(w2);
+                if (area > res) continue;
+
+                boolean point3 = false, point4 = false;
+                for (int[] p3 : points) {
+                    if (p3[0] == points[i][0] && p3[1] == points[j][1]) {
+                        point3 = true;
+                        break;
+                    }
+                }
+
+                for (int[] p4 : points) {
+                    if (p4[0] == points[j][0] && p4[1] == points[i][1]) {
+                        point4 = true;
+                        break;
+                    }
+                }
+
+                if (point3 && point4) res = area;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    public int minAreaRect(int[][] points) {
+        Map<Integer, HashSet<Integer>> map = new HashMap<>();
+        int ans = 0;
+        for (int[] a : points) {
+            if (!map.containsKey(a[0])) map.put(a[0], new HashSet<>());
+            map.get(a[0]).add(a[1]);
+        }
+
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++){
+            for (int j = i+1; j < points.length; j++) {
+                int[] a = points[i], b = points[j];
+                if (a[0] == b[0] || a[1] == b[1]) continue;
+                if (map.get(a[0]).contains(b[1]) && map.get(b[0]).contains(a[1])){
+                    res = Math.min(res, Math.abs((a[0] - b[0]) * (a[1] - b[1])));
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    private static int mod = 1000000007;
+
+    public int knightDialer(int N) {
+        int[][] dirs = new int[][] {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
+        int[][] dp = new int[N][10];
+        int ans = 0;
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int x : dirs[j]) {
+                    dp[i][x] += dp[i-1][j];
+                    dp[i][x] %= mod;
+                }
+            }
+        }
+
+        for (int x : dp[N-1]) {
+            ans += x;
+            ans %= mod;
+        }
+
+        return ans;
     }
 
     public int shortestBridge(int[][] A) {
@@ -181,7 +503,6 @@ public class Leet {
         return Math.min(start == s.length() - 1 ? 0 : cnt0[start + 1], 1 + minFlipsMonoIncrHelper(s, start + 1, cnt0));
     }
 
-    private static int mod = 1000000007;
     public int threeSumMulti(int[] a, int t) {
         int count = 0, n = a.length;
         for(int i=0; i < n; i++){
---
+++ src/main/java/com/orange/voice/test/TreeNode.java
@@ -0,0 +1,8 @@
+package com.orange.voice.test;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
